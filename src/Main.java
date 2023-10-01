74e29de5ef327da212568f48c3d15c8e5354304f
Jiří Korčák <j.korcak@centrum.cz>
2023-10-01 23:03:40 +0200

    Oprava LocalDate místo string.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.time.LocalDate;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         String name = "Tomáš Jedno";
-        String dateofbirth = "29.2.2000";
+        LocalDate dateofbirth = LocalDate.of(2000,2,29);
         int contract = 4;
         double carrotsold = 2.2;
         String SellerCity = "Brno";
